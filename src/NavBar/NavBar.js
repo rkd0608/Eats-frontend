4e8beba3509b05e5a4c92156606e78e434dc1e35
Rakshit Dwivedi <rkshtdwivedi8@gmail.com>
2020-10-09 01:36:36 +0530

    nav bar

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../assets/logo.png';
+import { SearchBar } from '../SearchBar/SearchBar';
+import styles from './NavBar.module.css';
+
+export function NavBar(){
+
+    return (
+        <div className={styles['nav-bar']}>
+            <img src = {logo} className={styles.logo} alt="belb_logo"></img>
+            <SearchBar small/>
+            <button className={`button ${styles['nav-button']}`}>Sign In</button>
+            <button className={`button ${styles['nav-button']}`}>Register</button>
+        </div>
+    );
+}
