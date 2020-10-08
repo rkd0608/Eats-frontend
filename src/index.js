96666dd97a6309c7e56cd9224e8571d649a9f276
Rakshit Dwivedi <rkshtdwivedi8@gmail.com>
2020-10-09 01:15:31 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bulma/css/bulma.css';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
